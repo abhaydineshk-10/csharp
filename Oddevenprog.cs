34da5f8c2f2b6d2d31e4a6c8eb77bc9099bea5de
Abhayy <eyeronic2772@gmail.com>
2025-07-31 14:56:34 +0530

    Add files via upload

---
+++ Oddevenprog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class OddEven
+{
+    public static void Main(string[] args)
+    {
+        int number = 10;
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Even");
+        }
+        else
+        {
+            Console.WriteLine("Odd");
+        }
+    }
+}
