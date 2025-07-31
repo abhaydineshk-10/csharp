96a9517069e79b17802df5566f0dfd067d506ffd
Abhayy <eyeronic2772@gmail.com>
2025-07-31 14:21:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace icecream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Icecream Land!");
+            Console.WriteLine("Please choose a flavor:");
+            Console.WriteLine("1. Vanilla");
+            Console.WriteLine("2. Chocolate");
+            Console.WriteLine("3. Strawberry");
+
+            string? choice = Console.ReadLine();
+            string flavor = choice switch
+            {
+                "1" => "Vanilla",
+                "2" => "Chocolate",
+                "3" => "Strawberry",
+                _ => "Unknown"
+            };
+
+            if (flavor == "Unknown")
+            {
+                Console.WriteLine("Invalid selection.");
+            }
+            else
+            {
+                Console.WriteLine($"You ordered: {flavor} ice cream. Enjoy!");
+            }
+        }
+    }
+}
