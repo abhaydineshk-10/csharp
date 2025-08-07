9834b83e323255633831e3fc19c5bc7318f16c98
Abhayy <eyeronic2772@gmail.com>
2025-08-07 15:00:49 +0530

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,35 @@
+﻿namespace DemoDataEncapsulation
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value > 0 && value < 100)
+                    age = value;
+                else
+                    Console.WriteLine("Invalid Age, Please enter age between 0 and 100");
+            }
+        }
+
+    }
+}
