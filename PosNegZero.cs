54cdb97fa8c2e71b0212002e98c357a4cdae54b9
Abhayy <eyeronic2772@gmail.com>
2025-09-18 14:12:01 +0530

    Add files via upload

---
+++ PosNegZero.cs
@@ -0,0 +1,28 @@
+﻿namespace PosNegZero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            if (double.TryParse(Console.ReadLine(), out double number))
+            {
+                Console.WriteLine(PosNegZero(number));
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+        }
+
+        static string PosNegZero(double num)
+        {
+            if (num > 0)
+                return "Positive";
+            else if (num < 0)
+                return "Negative";
+            else
+                return "Zero";
+        }
+    }
+}
